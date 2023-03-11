85a6a8ca327d72bc8d071db5cd4ddebc470f3eb5
AbuBkar-2000 <abdumannopovabubakir2000@mail.ru>
2023-03-11 18:10:02 +0500

    errors done

--- resources/js/Pages/Posts/Edit.vue
+++ resources/js/Pages/Posts/Edit.vue
@@ -8,9 +8,11 @@
             <div>
                 <input v-model="title" class="rounded-full mb-2 w-full border-gray-300" type="text" placeholder="Title">
             </div>
+            <div class="text-red-600 text-sm" v-if="errors">{{ errors.title}}</div>
             <div>
                 <textarea v-model="body" class="rounded-full w-full mb-2 border-gray-300" placeholder="Body"></textarea>
             </div>
+            <div class="text-red-600 text-sm" v-if="errors">{{ errors.body}}</div>
             <div class="flex">
                 <button
                     class="hover:bg-gray-100 hover:text-black mr-1 block p-2 w-3/4 bg-sky-500 border border-sky-500 rounded-full text-center text-white"
@@ -38,7 +40,7 @@ export default {
     },
 
     props:[
-        'post'
+        'post','errors',
     ],
 
     data(){
